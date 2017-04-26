23e623f35436ee36b23c34e85cfd2d4a7a693456
amit <amit@dell>
2017-04-26 23:15:13 +0300

    Update to limit the number of characters in password to 16 used hash instead of direct password

--- src/online/buzzzz/security/TextEncryptor.java
+++ src/online/buzzzz/security/TextEncryptor.java
@@ -7,21 +7,26 @@ package online.buzzzz.security;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import javafx.geometry.Insets;
 import javafx.application.Platform;
+import javafx.beans.property.StringProperty;
 import javafx.embed.swing.JFXPanel;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javax.swing.JApplet;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.xml.bind.DatatypeConverter;
 
 /**
  *
@@ -37,29 +42,39 @@ public class TextEncryptor extends JApplet {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
+        MessageDigest md;
+        byte[] hashBytes;
+        String hashString;
+        try{
+            md = MessageDigest.getInstance("SHA-256");
+            hashBytes = md.digest("test this is another test another test".getBytes());
+            System.out.println(hashBytes.length);
+            hashString = DatatypeConverter.printBase64Binary(hashBytes);
+            System.out.println(hashString.length());
+        }catch (NoSuchAlgorithmException ex){
             
-            @Override
-            public void run() {
-                try {
-                    UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
-                } catch (Exception e) {
-                }
-                
-                JFrame frame = new JFrame("Text Encryptor!");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setResizable(true);
-                JApplet applet = new TextEncryptor();
-                applet.init();
-                
-                frame.setContentPane(applet.getContentPane());
-                
-                frame.pack();
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
-                
-                applet.start();
+        }
+        
+        
+        SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+            } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException e) {
             }
+            
+            JFrame frame = new JFrame("Text Encryptor!");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setResizable(true);
+            JApplet applet = new TextEncryptor();
+            applet.init();
+            
+            frame.setContentPane(applet.getContentPane());
+            
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+            
+            applet.start();
         });
     }
     
@@ -69,12 +84,8 @@ public class TextEncryptor extends JApplet {
         fxContainer.setPreferredSize(new Dimension(JFXPANEL_WIDTH_INT, JFXPANEL_HEIGHT_INT));
         add(fxContainer, BorderLayout.CENTER);
         // create JavaFX scene
-        Platform.runLater(new Runnable() {
-            
-            @Override
-            public void run() {
-                createScene();
-            }
+        Platform.runLater(() -> {
+            createScene();
         });
     }
     
@@ -87,36 +98,33 @@ public class TextEncryptor extends JApplet {
         //source.setPrefColumnCount(400);
         source.setPrefRowCount(70);
         source.setWrapText(true);
-        TextField pass = new TextField();
+        TextField pass;
+        pass = new TextField();
         pass.setPrefColumnCount(200);
-        
+        pass.textProperty().addListener(
+                (observable,oldValue,newValue)->{
+                    if (newValue.length() > 16) ((StringProperty)observable).setValue(oldValue);
+                }
+        );
         
         HBox buttonArea = new HBox();
         //buttonArea.setPadding(defInset);
         buttonArea.setSpacing(10);
         Button encBtn = new Button();
         encBtn.setText("Encrypt");
-        encBtn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                String encrypted;
-                encrypted = AESCrypto.encrypt(pass.getText(), source.getText());
-                source.setText(encrypted);
-                //System.out.println("Hello World!");
-            }
+        encBtn.setOnAction((ActionEvent event) -> {
+            String encrypted;
+            encrypted = AESCrypto.encrypt(pass.getText(), source.getText());
+            source.setText(encrypted);
+            //System.out.println("Hello World!");
         });
         Button decBtn = new Button();
         decBtn.setText("Decrypt");
-        decBtn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                String plainText;
-                plainText = AESCrypto.decrypt(pass.getText(), source.getText());
-                source.setText(plainText);
-                //System.out.println("Hello World!");
-            }
+        decBtn.setOnAction((ActionEvent event) -> {
+            String plainText;
+            plainText = AESCrypto.decrypt(pass.getText(), source.getText());
+            source.setText(plainText);
+            //System.out.println("Hello World!");
         });
         buttonArea.getChildren().addAll(encBtn, decBtn);
         root.getChildren().addAll(source,pass,buttonArea);
